9fcf7d2e01b8fe9afe49bac0a5488dc405bddffc
Hafsa Iffath <132141581+HafsaIffath@users.noreply.github.com>
2025-08-24 10:48:46 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://hafsaiffath21:HafsaIffath21@cluster0.mvl5m9d.mongodb.net/food-del"
+      process.env.MONGODB_URI
     )
     .then(() => {
       console.log("MongoDB connected successfully");
